4736f7c7fb58c6cc0206592f6244f29cb1d6f839
jrached <juanrached@gmail.com>
2024-06-24 17:48:58 -0400

    Add PRM Plotting

--- navigation/path-planning/algorithms/prm.py
+++ navigation/path-planning/algorithms/prm.py
@@ -3,20 +3,14 @@
 import numpy as np
 from sklearn.neighbors import KDTree 
 from shapely.geometry import Point, LineString, Polygon 
+import matplotlib.pyplot as plt 
+import networkx as nx
 
-def dist_func(pnt1, pnt2):
-    return ((pnt2[0] - pnt1[0])**2 + (pnt2[1] - pnt1[2])**2)**0.5
+#TODO: Run A* on PRM and plot output.
 
-def get_random_point(size):
-    return np.random.rand(2) * size
-
-def nearest_neighbors(pnt, pnts, tree, n):
-    """
-    Gets n nearest neighbors of pnt from a KDTree object.
-    """
-    _, indices = tree.query([pnt], k=n+1)
-    return pnts[indices][:,1:,:][0]
-
+#########################
+###Collision Functions###
+#########################
 def point_in_collision(pnt, obstacles):
     point, collision = Point(pnt), False
     for obstacle in obstacles: 
@@ -31,52 +25,87 @@ def segment_in_collision(segment, obstacles):
         collision = collision or polygon.intersects(line_segment)
     return collision 
 
-def build_prm(k, grid_size, obstacles, n=5):
-    #TODO: Change numpy arrays to lists
-    v, e = np.array([[0,0]]), np.array([[0,0]])
-    for _ in range(k):
+####################
+###Main Functions###
+####################
+def get_random_point(size):
+    x, y = np.random.rand(2) * size
+    return (x, y)
+
+def nearest_neighbors(pnt, pnts, tree, k):
+    """
+    Gets n nearest neighbors of pnt from a KDTree object.
+    """
+    pnts = np.array(pnts)
+    _, indices = tree.query([pnt], k=k+1)
+    neighbors = pnts[indices][0, 1:,:]
+    return [tuple(neighbor) for neighbor in neighbors]
+
+def build_prm(iters, grid_size, obstacles, k):
+    v, e = [], []
+    for _ in range(iters):
         pnt = get_random_point(grid_size)
         while point_in_collision(pnt, obstacles):
             pnt = get_random_point(grid_size)
-        v = np.vstack((v, pnt))
+        v.append(pnt)
 
     tree = KDTree(v, leaf_size=2)
     for pnt in v: 
-        for neighbor in nearest_neighbors(pnt, v, tree, n):
+        for neighbor in nearest_neighbors(pnt, v, tree, k):
                 edge = [pnt, neighbor]
                 if not segment_in_collision(edge, obstacles):
-                    np.vstack((e, edge))
+                    e.append(edge)
     
     return v, e
     
+##############
+###Plotting###
+##############
+def convert_to_nx(v, e):
+    graph = nx.Graph()
+    for vertex in v:
+        graph.add_node(vertex)
+    for edge in e:
+        graph.add_edge(*edge)
+    
+    pos = {node : node for node in graph.nodes}
+    return graph, pos
+
+def plot_prm(v, e):
+    nx_graph, pos = convert_to_nx(v, e)
+    
+    plt.title('Probabilistic Roadmap')
+    nx.draw(nx_graph, pos, node_size=10, node_color="red", edge_color="orange")
+
+    #plot obstacles
+    for obstacle in obstacles: 
+        obstacle.append(obstacle[0]) #complete plot
+        x, y = [], []
+        for segment in obstacle:
+            x.append(segment[0])
+            y.append(segment[1])
+        plt.plot(x, y, color="blue")
+
+    #Plot environment boundary
+    x, y = [0, 0, 100, 100, 0], [0, 100, 100, 0, 0]
+    plt.plot (x, y, color="blue")
+
+    plt.show()
+
 
 if __name__ == '__main__': 
-    # points = np.array([(i, j) for i, j in zip(range(10),range(10,20))])
-    # print(points)
-    # tree = KDTree(points, leaf_size=2)
-
-    # query_point = (1, 11)
-    # nn = nearest_neighbors(query_point, points, tree, 10)
-    # print(nn)
-
-    obstacles = [[(2,4), (4,2), (6,4), (4,6)]]
-    # point = np.array((3,3))
-    # point_collision = point_in_collision(point, obstacles)
-
-    # line = [(2,2), (2.5, 2.5)]
-    # line_collision = segment_in_collision(line, obstacles)
-
-    # print(point_collision)
-
-    iters = 200
+    #PRM parameters
+    iters = 500
     grid_size = 100
+    k = 20
 
-    v, e = build_prm(iters, grid_size, obstacles)
+    #Define obstacles
+    obstacles = []
+    for center in [(40, 40), (20, 20), (20, 80), (70, 70), (60, 10)]:
+        obstacle = [(center[0]+dx, center[1]+dy) for dx, dy in [(0, 10), (10, 20), (20, 10), (10, 0)]]
+        obstacles.append(obstacle)
 
-    # arr1 = np.array([1, 2, 3])
-    # arr2 = np.array([4, 5, 6])
-    # arr3 = np.vstack((arr1, arr2))
-
-    # print(arr3)
+    vertices, edges = build_prm(iters, grid_size, obstacles, k)
+    plot_prm(vertices, edges)
 
 
