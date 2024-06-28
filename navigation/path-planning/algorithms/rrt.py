052c575d2faddfd6afbd71142bf3a5b6f7f6f2f7
jrached <juanrached@gmail.com>
2024-06-28 12:16:45 -0400

    Implement RRT Goal Bias

--- navigation/path-planning/algorithms/rrt.py
+++ navigation/path-planning/algorithms/rrt.py
@@ -14,9 +14,11 @@ from utils import shortcutting, get_segment_intersection, Map
 def distance_func(pnt1, pnt2):
     return ((pnt2[0] - pnt1[0])**2 + (pnt2[1] - pnt1[1])**2)**0.5
 
-def get_rand_point(map_size):
-    rand_xy = np.random.rand(2) * map_size
-    return (rand_xy[0], rand_xy[1])
+def get_rand_point(map_size, goal, prob=0.2):
+    x, y = np.random.rand(2) * map_size
+    if np.random.rand(1)[0] < prob:
+        x, y = goal
+    return (x, y)
 
 def nearest_point(g, rand_pnt): #Runtime: O(n)
     return min(g.keys(), key=lambda x: distance_func(x, rand_pnt))
@@ -41,13 +43,10 @@ def rrt(g, netx, num_iters, step, map_size, obstacles, goal_node, eps): #Runtime
                 A networkx graph object for plotting the tree, netx. And an array
                 containing all the nodes that are within a distance of the goal, paths.
     """
-    paths = []
     for _ in range(num_iters):
-        random_point = get_rand_point(map_size)
+        random_point = get_rand_point(map_size, goal_node)
         nearest = nearest_point(g, random_point)
         new_node = get_new_node(nearest, random_point, step)
-
-        #Check for collisions 
         new_node = get_closest_collision(obstacles, nearest, new_node)
 
         if new_node != nearest:
@@ -60,28 +59,25 @@ def rrt(g, netx, num_iters, step, map_size, obstacles, goal_node, eps): #Runtime
 
             #Check if goal reached
             if distance_func(new_node, goal_node) < eps:
-                paths.append(new_node)
+                break
 
-    return g, netx, paths
+    return g, netx, new_node
 
-def reconstruct_path(g, paths):
+def reconstruct_path(g, end_node):
     """
-    For each node that is within a distance of the goal, end_node,
+    For a node that is within a distance of the goal, end_node,
     it goes back up its parents until it reaches the starting node.
 
     Input(s): A dictionary mapping all nodes to their immidiate parent, g, 
-                and an array containing all the end nodes, paths.
+                and the end of the successful path, end_node.
     Outputs(s): An array containing the shortest path found.
     """
-    full_paths = []
-    for end_node in paths:
-        full_path = []
-        while end_node: 
-            full_path.append(end_node)
-            end_node = g[end_node]
-        full_paths.append(full_path)
+    path = []
+    while end_node: 
+        path.append(end_node)
+        end_node = g[end_node]
 
-    return full_paths if full_paths == [] else min(full_paths, key=lambda x: len(x))[::-1]
+    return path[::-1]
 
 #########################
 ###Collision Functions###
@@ -112,6 +108,7 @@ def get_closest_collision(obstacles, nearest, new_node):
     Get the closest intersection between any of the line segments 
     of any of the obstacles in the field and the line formed by the 
     nearest point on the tree and the point we're extending the tree towards.
+    If there isn't any, it just returns the new node.
 
     Input(s): A list of polygons, obstacles.
               The nearest point on the tree as a tuple, nearest.
@@ -119,11 +116,11 @@ def get_closest_collision(obstacles, nearest, new_node):
     Output(s): The closest intersection out of all the obstacles and our line of interest.
                If there isn't any, the output will be new_node.               
     """
-    collisions = []
+    collisions = [new_node]
     for obstacle in obstacles: 
         collision = get_obstacle_intersection(obstacle, nearest, new_node)
         collisions.append(collision)
-    
+
     cc = min(collisions, key=lambda x: distance_func(x, nearest))
     if cc != new_node: #Avoid python rounding error at edges of obstacle.
         mag = distance_func(nearest, cc)
@@ -191,8 +188,9 @@ if __name__ == "__main__":
     netx_g = nx.Graph()
     netx_g.add_node(start_node)
 
-    new_graph, new_g, paths = rrt(graph, netx_g, iters, step_size, map_size, obstacles, goal, eps)
-    best_path = reconstruct_path(new_graph, paths) 
+    new_graph, new_g, end_node = rrt(graph, netx_g, iters, step_size, map_size, obstacles, goal, eps)
+    
+    best_path = reconstruct_path(new_graph, end_node) 
     best_path = shortcutting(best_path + [goal], obstacles)
     
     plot_graph(new_g, map_size, obstacles, start_node, goal, best_path)
